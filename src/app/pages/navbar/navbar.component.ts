e7de50fc9ffc3a7a11bb754d78e3c3354cd5bfdf
Ralph Tanaka Mapuranga <ralphtmapuranga@gmail.com>
2024-09-30 10:04:06 +0200

    Final UI
    Final UI

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -6,14 +6,29 @@ import { MatButtonModule} from '@angular/material/button';
 import { RegisterComponent } from '../register/register.component';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router'; 
+import { MatSidenav,MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatNavList } from '@angular/material/list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatDividerModule, MatButtonModule, RegisterComponent, RouterOutlet, RouterLink],
+  imports: [CommonModule, MatNavList, MatSidenavContainer, MatSidenav, MatSidenavContent, MatToolbarModule, MatIconModule, MatDividerModule, MatButtonModule, RegisterComponent, RouterOutlet, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+sidenav: any;
+
+isSmallScreen: boolean = false;
+
+constructor(private breakpointObserver: BreakpointObserver) {
+  this.breakpointObserver.observe([Breakpoints.Handset])
+    .subscribe(result => {
+      this.isSmallScreen = result.matches;
+    });
+}
 
 }
